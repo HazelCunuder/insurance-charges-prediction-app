e8b2214ea4d520ac65a0c5bdd27b8a57daa7485e
Flora T <flora.trecul@gmail.com>
2026-01-28 11:56:17 +0100

    Feat: add basic form on predict page

--- InsuranceChargePredictionApp/InsuranceChargePredictionApp/urls.py
+++ InsuranceChargePredictionApp/InsuranceChargePredictionApp/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", HomePageView.as_view(), name="home")
+    path("", HomePageView.as_view(), name="home"),
+    path('predict/', include('predict.urls')),
 ]
 
 if settings.DEBUG:
