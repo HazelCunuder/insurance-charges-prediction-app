88c4d37c775107805f36d5282de00beffc7e4464
Hazel Cunuder <122833349+HazelCunuder@users.noreply.github.com>
2026-01-28 12:07:54 +0100

    fix: remove url

--- InsuranceChargePredictionApp/InsuranceChargePredictionApp/urls.py
+++ InsuranceChargePredictionApp/InsuranceChargePredictionApp/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", HomePageView.as_view(), name="home"),
-    path('predict/', include('predict.urls')),
+
 ]
 
 if settings.DEBUG:
