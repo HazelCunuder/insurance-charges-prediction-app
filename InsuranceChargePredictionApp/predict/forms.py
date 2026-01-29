82a36f32ec603b3b82def7f40a1314fba95e564f
Flora T <flora.trecul@gmail.com>
2026-01-29 12:20:00 +0100

    Feat: prefill prediction form with registered accounts infos

--- InsuranceChargePredictionApp/predict/forms.py
+++ InsuranceChargePredictionApp/predict/forms.py
@@ -5,11 +5,11 @@ class PredictionForm(forms.Form):
     SMOKER_CHOICES = [('yes', 'Oui'), ('no', 'Non')]
     REGION_CHOICES = [('northeast', 'Nord-Est'), ('northwest', 'Nord-Ouest'), ('southeast', 'Sud-Est'), ('southwest', 'Sud-Ouest')]
 
-    age = forms.IntegerField(label='Âge :', min_value=18, max_value=120, required=True)
+    age = forms.IntegerField(label='Âge :', min_value=18, max_value=125, required=True)
     gender = forms.ChoiceField(label='Genre :', choices=GENDER_CHOICES, required=True)
 
     weight = forms.FloatField(label='Poids (en kg) :', min_value=30, max_value=250, required=True)
-    height = forms.FloatField(label='Taille (en m) :', min_value=1, max_value=2.20, required=True)
+    height = forms.FloatField(label='Taille (en m) :', min_value=1, max_value=2.50, required=True)
 
     smoker = forms.ChoiceField(label='Fumeur :', choices=SMOKER_CHOICES, required=True)
     children = forms.IntegerField(label='Nombre d\'enfants :', min_value=0, max_value=10, required=True)
