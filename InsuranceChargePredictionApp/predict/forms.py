8bf772e6232859d623824bd319bf435cec2a7316
Flora T <flora.trecul@gmail.com>
2026-01-30 10:30:44 +0100

    Feat: add custom error messages to all forms

--- InsuranceChargePredictionApp/predict/forms.py
+++ InsuranceChargePredictionApp/predict/forms.py
@@ -8,34 +8,34 @@ class PredictionForm(forms.Form):
     first_name = forms.CharField(
         label="Prénom", 
         min_length=2,
-        max_length=50, 
-        required=True,
+        max_length=50,
         error_messages={
             'required': 'Veuillez renseigner un prénom.',
-            'min_length': 'Le prénom doit comporter au moins 3 lettres.',
+            'min_length': 'Le prénom doit comporter au moins 2 caractères.',
             'max_length': 'Le prénom ne peut pas contenir plus de 50 caractères.'
             })
 
     last_name = forms.CharField(
-        label='Nom de famille', 
+        label='Nom', 
         min_length=2,
-        max_length=50, 
-        required=True,
+        max_length=50,
         error_messages={
             'required': 'Veuillez renseigner un nom de famille.',
-            'min_length': 'Le nom doit comporter au moins 3 lettres.',
+            'min_length': 'Le nom doit comporter au moins 2 caractères.',
             'max_length': 'Le nom ne peut pas contenir plus de 50 caractères.'
         })
 
     email = forms.EmailField(
-        label='Adresse mail'
-    )
+        label='Adresse email',
+        error_messages={
+            'required': "Veuillez renseigner une adresse email.",
+            'invalid': "Veuillez renseigner une adresse email valide."
+        })
 
     age = forms.IntegerField(
         label='Âge :', 
         min_value=18, 
-        max_value=125, 
-        required=True, 
+        max_value=125,
         error_messages={
             'required': 'Veuillez renseigner un âge.',
             'min_value': 'Le client doit avoir 18 ans minimum.',
@@ -45,16 +45,17 @@ class PredictionForm(forms.Form):
 
     gender = forms.ChoiceField(
         label='Genre :', 
-        choices=GENDER_CHOICES, 
-        required=True,
-        )
+        choices=GENDER_CHOICES,
+        error_messages={
+            'required': 'Veuillez sélectionner un genre.',
+            'invalid_choice': 'Ce choix n\'est pas valide.'
+        })
 
     weight = forms.FloatField(
         label='Poids (en kg) :', 
         min_value=30, 
         max_value=250, 
-        step_size = 0.01,
-        required=True,
+        step_size = 0.1,
         error_messages={
             'required': 'Veuillez renseigner un poids.',
             'min_value': 'Le poids ne peut pas être inférieur à 30 kg.',
@@ -67,7 +68,6 @@ class PredictionForm(forms.Form):
         min_value=1, 
         max_value=2.50, 
         step_size = 0.01,
-        required=True,
         error_messages={
             'required': 'Veuillez renseigner une taille.',
             'min_value': 'La taille ne peut pas être inférieure à 1 mètre.',
@@ -77,15 +77,16 @@ class PredictionForm(forms.Form):
 
     smoker = forms.ChoiceField(
         label='Fumeur :', 
-        choices=SMOKER_CHOICES, 
-        required=True
-        )
+        choices=SMOKER_CHOICES,
+        error_messages={
+            'required': 'Veuillez sélectionner une option.',
+            'invalid_choice': 'Ce choix n\'est pas valide.'
+        })
 
     children = forms.IntegerField(
         label='Nombre d\'enfants :', 
         min_value=0, 
-        max_value=15, 
-        required=True,
+        max_value=15,
         error_messages={
             'required': 'Veuillez renseigner le nombre d\'enfants.',
             'min_value': 'Le nombre d\'enfants ne peut pas être négatif.',
@@ -95,5 +96,8 @@ class PredictionForm(forms.Form):
 
     region = forms.ChoiceField(
         label='Région :', 
-        choices=REGION_CHOICES
-        )
+        choices=REGION_CHOICES,
+        error_messages={
+            'required': 'Veuillez sélectionner une région.',
+            'invalid_choice': 'Ce choix n\'est pas valide.'
+        })
